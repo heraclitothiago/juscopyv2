cab65f8432f79d9095fea4fdf9013fd87c614f8f
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-10-18 22:57:31 -0300

    código atualizado

--- final.js
+++ final.js
@@ -5,7 +5,7 @@ document.head.appendChild(swal);
 var url = () => window.location;
 
 var regex = target => {
-    return "https:\/\/.*\.jusbrasil.com.br\/" + target + "\/[0-9]*\/.*"
+    return "https:\/\/.*\.jusbrasil.com.br\/" + target + "\/[0-9]*\/.*";
 };
 
 var modalAlerts = {
@@ -30,47 +30,41 @@ var modalAlerts = {
 }
 
 function juscopyBtn(father) {
-    //Cria um botão novo
-    var btnJuscopy = document.createElement('a')
-    btnJuscopy.classList.add('btn')
-    btnJuscopy.classList.add('btn--orange')
-    btnJuscopy.setAttribute("id", "juscopy")
-    btnJuscopy.innerText = "Copiar com Juscopy"
-        //adiciona o ícone ao botão
+    var btnJuscopy = document.createElement('a');
+    btnJuscopy.classList.add('btn');
+    btnJuscopy.classList.add('btn--orange');
+    btnJuscopy.setAttribute("id", "juscopy");
+    btnJuscopy.innerText = "Copiar com Juscopy";
     var icon = document.createElement('span');
     icon.classList.add('icon');
     icon.classList.add('icon-content-copy');
-    btnJuscopy.appendChild(icon)
-    divToAppend = document.querySelector(father)
-    divToAppend.appendChild(btnJuscopy)
+    btnJuscopy.appendChild(icon);
+    divToAppend = document.querySelector(father);
+    divToAppend.appendChild(btnJuscopy);
 }
 
 var removeBtn = selector => {
-    var btn = document.querySelector(selector)
-    btn.remove()
+    var btn = document.querySelector(selector);
+    btn.remove();
 }
 
 if (url().href.match(regex('modelos-pecas'))) {
-    //Remove os botões download e copiar da jurisprudência
-    removeBtn(".DocumentActionsCard-download-btn")
-    removeBtn(".DocumentActionsCard-copy-btn")
-
+    removeBtn(".DocumentActionsCard-download-btn");
+    removeBtn(".DocumentActionsCard-copy-btn");
     juscopyBtn(".DocumentActionsCard-actions");
-
 } else if (url().href.match(regex('processos'))) {
-    removeBtn("button.btn--blue:nth-child(1)")
+    removeBtn("button.btn--blue:nth-child(1)");
     juscopyBtn(".ToolBarBase-leftActions");
-
 } else if (url().href.match(regex('jurisprudencia'))) {
     try {
         var btn = document.querySelector("button.btn--blue");
-        btn.click()
+        btn.click();
     } catch (e) {}
-    removeBtn(".CopyContentModal-copyButton")
-    juscopyBtn(".modal-footer")
-
+    removeBtn(".CopyContentModal-copyButton");
+    juscopyBtn(".modal-footer");
 } else {
-    throw new console.error("Você não está no ambiente correto do jusbrasil");
+    Swal.fire(modalAlerts.fail)
+        // throw new console.error("Você não está no ambiente correto do jusbrasil");
 }
 
 var selectors = [];
@@ -80,7 +74,7 @@ selectors['modelos-pecas'] = ".unprintable";
 
 var btnCopy = document.querySelector("#juscopy");
 btnCopy.addEventListener("click", function() {
-    var base = window.location.pathname.split("/")
+    var base = url().pathname.split("/")
     var content = document.querySelector(selectors[base[1]])
     navigator.clipboard.writeText(content.innerText)
     Swal.fire(modalAlerts.success)
@@ -90,5 +84,6 @@ btnCopy.addEventListener("click", function() {
         var btnFechar = document.querySelector(".CopyContentModal-closeButton");
         btnFechar.click()
     } catch (e) {}
-    console.log('Copiado com sucesso! Faça-nos um pix e apoie o nosso projeto e-mail dradvloper@gmail.com');
+    console.log('%c Copiado com sucesso! Faça-nos um pix e apoie o nosso projeto e-mail dradvloper@gmail.com',
+        'background: #ee8500; color: black; font-size: 15px');
 })
